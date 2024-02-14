64dc75bbe5f92df7d29921543ffe8700361dcfef
JohnDoe-Jack <makaroni.aori0421@gmail.com>
2024-02-14 23:32:12 +0900

    Feature geocode

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,9 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-
-/**
- * 緯度 34.86271544710158
- * 経度 135.68093024881938
- */
+import axios from 'axios';
 
 /* エラーテキスト */
 const ErrorText = () => (
@@ -13,9 +9,10 @@ const ErrorText = () => (
 const Location = () => {
   const [isAvailable, setAvailable] = useState(false);
   const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
-
   // useEffectが実行されているかどうかを判定するために用意しています
   const isFirstRef = useRef(true);
+
+  const [coordinates, setCoordinates] = useState(null);
 
   /*
    * ページ描画時にGeolocation APIが使えるかどうかをチェックしています
@@ -34,6 +31,19 @@ const Location = () => {
       setPosition({ latitude, longitude });
     });
   };
+  const handleGeocode = async (latitude: number, longitude: number) => {
+    try {
+      const response = await axios.get(
+        `https://map.yahooapis.jp/geoapi/V1/reverseGeoCoder?lat=${latitude}&lon=${longitude}&appid=${
+          import.meta.env.YAHOO_API
+        }`
+      );
+      console.log(response.data);
+      setCoordinates(response.data);
+    } catch (error) {
+      console.error('Error fetching geocoding data:', error);
+    }
+  };
 
   // useEffect実行前であれば、"Loading..."という呼び出しを表示させます
   if (isFirstRef.current) return <div className="App">Loading...</div>;
@@ -45,10 +55,21 @@ const Location = () => {
       {isAvailable && (
         <div>
           <button onClick={getCurrentPosition}>Get Current Position</button>
+          <br />
+          <button
+            onClick={() => handleGeocode(34.86271544710158, 135.68093024881938)}
+          >
+            Get Geocode
+          </button>
+          <br />
+          latitude: {position.latitude}
+          <br />
+          longitude: {position.longitude}
+          <br />
           <div>
-            latitude: {position.latitude}
-            <br />
-            longitude: {position.longitude}
+            {coordinates && (
+              <p>Address: {coordinates.Feature[0].Property.Address}</p>
+            )}
           </div>
         </div>
       )}
