78cff8df5fccc29db1f1e929f6f3fae04d1d6f42
JohnDoe-Jack <makaroni.aori0421@gmail.com>
2024-02-16 11:12:18 +0900

    Add ReverseGeoCoder API

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,80 +1,38 @@
-import { useState, useEffect, useRef } from 'react';
+// ReverseGeocoding.tsx
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-/* エラーテキスト */
-const ErrorText = () => (
-  <p className="App-error-text">geolocation IS NOT available</p>
-);
+interface GeoResponse {
+  Feature: {
+    Property: {
+      Address: string;
+    };
+  }[];
+}
 
-const Location = () => {
-  const [isAvailable, setAvailable] = useState(false);
-  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
-  // useEffectが実行されているかどうかを判定するために用意しています
-  const isFirstRef = useRef(true);
+const ReverseGeocoding: React.FC = () => {
+  const [address, setAddress] = useState<string>('');
 
-  const [coordinates, setCoordinates] = useState(null);
-
-  /*
-   * ページ描画時にGeolocation APIが使えるかどうかをチェックしています
-   * もし使えなければその旨のエラーメッセージを表示させます
-   */
   useEffect(() => {
-    isFirstRef.current = false;
-    if ('geolocation' in navigator) {
-      setAvailable(true);
-    }
-  }, [isAvailable]);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<GeoResponse>(
+          `http://localhost:3000/reverse-geocode?lat=35.681236&lon=139.767125`
+        );
+        setAddress(response.data.Feature[0].Property.Address);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  const getCurrentPosition = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords;
-      setPosition({ latitude, longitude });
-    });
-  };
-  const handleGeocode = async (latitude: number, longitude: number) => {
-    try {
-      const response = await axios.get(
-        `https://map.yahooapis.jp/geoapi/V1/reverseGeoCoder?lat=${latitude}&lon=${longitude}&appid=${
-          import.meta.env.YAHOO_API
-        }`
-      );
-      console.log(response.data);
-      setCoordinates(response.data);
-    } catch (error) {
-      console.error('Error fetching geocoding data:', error);
-    }
-  };
-
-  // useEffect実行前であれば、"Loading..."という呼び出しを表示させます
-  if (isFirstRef.current) return <div className="App">Loading...</div>;
+    fetchData();
+  }, []);
 
   return (
-    <div className="App">
-      <p>Geolocation API Sample</p>
-      {!isFirstRef && !isAvailable && <ErrorText />}
-      {isAvailable && (
-        <div>
-          <button onClick={getCurrentPosition}>Get Current Position</button>
-          <br />
-          <button
-            onClick={() => handleGeocode(34.86271544710158, 135.68093024881938)}
-          >
-            Get Geocode
-          </button>
-          <br />
-          latitude: {position.latitude}
-          <br />
-          longitude: {position.longitude}
-          <br />
-          <div>
-            {coordinates && (
-              <p>Address: {coordinates.Feature[0].Property.Address}</p>
-            )}
-          </div>
-        </div>
-      )}
+    <div>
+      <p>Address: {address}</p>
     </div>
   );
 };
 
-export default Location;
+export default ReverseGeocoding;
